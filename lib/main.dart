d45d83202126c9c31813f533d77371fed1f5ccf8
WSixx <lucas.2007s@gmail.com>
2021-03-31 12:03:41 -0300

    resources

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'My Rider APP',
       theme: ThemeData(
+          fontFamily: "Signatra",
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity),
       home: HomeScreen(),
